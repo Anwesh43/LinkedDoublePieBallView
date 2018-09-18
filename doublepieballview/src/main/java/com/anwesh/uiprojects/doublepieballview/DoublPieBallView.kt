fee5e1384c10dde99b46a2a3084b982961da21ec
Anwesh43 <anweshthecool0@gmail.com>
2018-09-18 10:07:53 +0530

    adding libraries and defining nodes

--- doublepieballview/src/main/java/com/anwesh/uiprojects/doublepieballview/DoublPieBallView.kt
+++ doublepieballview/src/main/java/com/anwesh/uiprojects/doublepieballview/DoublPieBallView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.doublepieballview
 /**
  * Created by anweshmishra on 18/09/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+
+val nodes : Int = 5
